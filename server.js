f2cb3dc59d413c3982397acbd7a1e2c080ded6b5
mandeepharsh <100046979+mandeepharsh@users.noreply.github.com>
2022-09-04 17:14:33 -0700

    serverjs

--- server.js
+++ server.js
@@ -12,11 +12,10 @@ const image = require('./controllers/image.js')
 const db= knex({
     client: 'pg',
     connection: {
-connectionString: process.env.DATABASE_URL,
-ssl: {
-rejectUnauthorized: false,
-},
-});
+      connectingString : process.env.DATABASE_URL,
+      ssl : true,
+    }
+  });
 
 db.select('*').from('users')
 app.use(express.json());
