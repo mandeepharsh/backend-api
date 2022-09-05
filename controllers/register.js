90756906d476a7bc35989fc8a1ea912cea5b6109
mandeepharsh <100046979+mandeepharsh@users.noreply.github.com>
2022-09-04 18:31:52 -0700

    status co

--- controllers/register.js
+++ controllers/register.js
@@ -24,7 +24,7 @@ const handleRegister = (req,res,db ,bcrypt)=>{
     .then(trx.commit)
     .catch(trx.rollback);
  })
-   .catch(err => res.status(400).json("err"))
+   .catch(err => res.status(400).json(err))
 }
 module.exports = {
     handleRegister : handleRegister
